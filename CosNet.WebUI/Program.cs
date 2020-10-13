60af348c9ad14bf7e7dc15c0698edcd9be15ba6b
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-10-13 21:15:37 +0200

    replaced fetch data example page with data from backend

--- CosNet.WebUI/Program.cs
+++ CosNet.WebUI/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace CosNet.WebUI
 {
@@ -18,6 +19,7 @@ namespace CosNet.WebUI
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped<HttpClient>();
+            
 
             builder.Services.AddOidcAuthentication(options =>
             {
