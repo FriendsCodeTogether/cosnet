a1d40e0529492055dcb8247ccf3bae59a535ce68
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-10-13 16:05:01 +0200

    fixed formatting

--- CosNet.API/DbContexts/DbSeeder.cs
+++ CosNet.API/DbContexts/DbSeeder.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using CosNet.API.Models;
-
 using Microsoft.EntityFrameworkCore;
 
 namespace CosNet.API.DBContexts
--- CosNet.API/Models/Cosplay.cs
+++ CosNet.API/Models/Cosplay.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Microsoft.VisualBasic;
 
 namespace CosNet.API.Models
