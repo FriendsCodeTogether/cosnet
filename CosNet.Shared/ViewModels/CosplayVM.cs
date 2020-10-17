b7da4f96aa5d8a45206655e7977e5ecff2853703
DaSernet <Daanseijnhaeve@gmail.com>
2020-10-17 20:48:34 +0200

    Added cosplayVM

--- CosNet.Shared/ViewModels/CosplayVM.cs
+++ CosNet.Shared/ViewModels/CosplayVM.cs
@@ -6,5 +6,13 @@ namespace CosNet.Shared.ViewModels
 {
    public class CosplayVM
    {
+      public Guid Id { get; set; }
+      public Guid UserId { get; set; }
+      public string Name { get; set; }
+      public string Serie { get; set; }
+      public DateTime StartDate { get; set; }
+      public DateTime DueDate { get; set; }
+      public decimal Budget { get; set; }
+      public string Status { get; set; }
    }
 }
