2650ef1c8757ea810ff5221951ff82aeeaa4289e
Kaatje Schillemans <kaatschillemans@hotmail.com>
2020-10-18 14:36:25 +0200

    added cosplay model

--- CosNet.AppUI.Android/app/src/main/java/cosnet/android/Models/Cosplay.java
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/Models/Cosplay.java
@@ -1,4 +1,24 @@
 package cosnet.android.Models;
 
+import java.util.Date;
+
 public class Cosplay {
+
+   private enum statuses {
+      Planned,
+      InProgress,
+      Completed
+   }
+
+   public String Name;
+   public String Serie;
+   public Date StartDate;
+   public Date DueDate;
+   public double Budget;
+   public statuses Status;
+   //public Collection<Note> Notes;
+   //Item property.
+   //ref pictures property
+   //Progress pictures property
+   //After pictures property
 }
