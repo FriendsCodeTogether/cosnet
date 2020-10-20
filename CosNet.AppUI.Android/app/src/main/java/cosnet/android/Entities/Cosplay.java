859c77e77d1df7afed234b3596d9d62d431db998
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-10-20 23:09:29 +0200

    changed property names to follow java conventions

--- CosNet.AppUI.Android/app/src/main/java/cosnet/android/Entities/Cosplay.java
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/Entities/Cosplay.java
@@ -12,20 +12,27 @@ public class Cosplay {
    @PrimaryKey(autoGenerate = true)
    public int id;
 
-   @NonNull public String cosplay_name;
-   @Nullable public String cosplay_series;
-   @Nullable public String start_date;
-   @Nullable public String due_date;
+   @ColumnInfo(name = "cosplay_name")
+   @NonNull public String cosplayName;
+
+   @ColumnInfo(name = "cosplay_series")
+   @Nullable public String cosplaySeries;
+
+   @ColumnInfo(name = "start_date")
+   @Nullable public String startDate;
+
+   @ColumnInfo(name = "due_date")
+   @Nullable public String dueDate;
+
    @Nullable public Double budget;
    @NonNull public String status;
 
-   public Cosplay(@NonNull String cosplay_name, @Nullable String cosplay_series, @Nullable String start_date, @Nullable String due_date, @Nullable Double budget,@NonNull String status){
-      this.cosplay_name = cosplay_name;
-      this.cosplay_series = cosplay_series;
-      this.start_date = start_date;
-      this.due_date = due_date;
+   public Cosplay(@NonNull String cosplayName, @Nullable String cosplaySeries, @Nullable String startDate, @Nullable String dueDate, @Nullable Double budget,@NonNull String status){
+      this.cosplayName = cosplayName;
+      this.cosplaySeries = cosplaySeries;
+      this.startDate = startDate;
+      this.dueDate = dueDate;
       this.budget = budget;
       this.status = status;
    }
-
 }
