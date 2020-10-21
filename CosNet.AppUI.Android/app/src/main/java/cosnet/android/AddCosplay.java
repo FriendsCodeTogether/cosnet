1b72e3ea344669e56e49627e24fba3c19db2f99e
Tim Blokken <tim_blokken@hotmail.com>
2020-10-21 14:26:53 +0200

    cleaned up code

--- CosNet.AppUI.Android/app/src/main/java/cosnet/android/AddCosplay.java
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/AddCosplay.java
@@ -33,149 +33,147 @@ import java.util.Locale;
 import cosnet.android.Models.Cosplay;
 import me.abhinay.input.CurrencyEditText;
 
-public class AddCosplay extends Activity {
+public class AddCosplay extends Activity{
+
    private static final String TAG = "AddCosplay";
+   DatePickerDialog.OnDateSetListener setListener;
+
+   private EditText characterEditText;
+   private EditText seriesEditText;
+   private EditText startDateEditText;
+   private EditText dueDateEditText;
+   private CurrencyEditText budgetEditText;
+   private Spinner statusSpinner;
+   private ImageButton cancelButton;
+   private Button addCosplayButton;
+   private List<String> statusses;
+   private int year;
+   private int month;
+   private int day;
+
    @Override
    protected void onCreate( Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.add_cosplay_main);
 
-      Log.d(TAG, "onCreate: Starting.");
-      ImageButton cancelNewButton = (ImageButton) findViewById(R.id.cancelNewCosplayBTN);
-      cancelNewButton.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-            Log.d(TAG, "onClick: clicked cancelBUttonBTN");
+      Log.d(TAG, "onCreate: Starting");
 
-            Intent intent = new Intent(AddCosplay.this, MainActivity.class);
-            startActivity(intent);
-         }
-      });
-
-      final EditText characterEditText = (EditText)findViewById(R.id.characterEditText) ;
-      final EditText seriesEditText = (EditText)findViewById(R.id.seriesEditText) ;
-      final EditText startDateEditText = (EditText)findViewById(R.id.startDateEditText) ;
-      final EditText dueDateEditText = (EditText)findViewById(R.id.dueDateEditText) ;
-      final CurrencyEditText budgetEditText = (CurrencyEditText)findViewById(R.id.budgetEditText) ;
-      final Spinner statusSpinner = (Spinner)findViewById(R.id.statusSpinner) ;
+      characterEditText = (EditText)findViewById(R.id.characterEditText) ;
+      seriesEditText = (EditText)findViewById(R.id.seriesEditText) ;
+      startDateEditText = (EditText)findViewById(R.id.startDateEditText) ;
+      dueDateEditText = (EditText)findViewById(R.id.dueDateEditText) ;
+      budgetEditText = (CurrencyEditText)findViewById(R.id.budgetEditText) ;
+      statusSpinner = (Spinner)findViewById(R.id.statusSpinner) ;
+      cancelButton = (ImageButton) findViewById(R.id.cancelNewCosplayBTN);
+      addCosplayButton = (Button) findViewById(R.id.addCosplayBTN);
       Calendar calendar = Calendar.getInstance();
-      final int year = calendar.get(Calendar.YEAR);
-      final int month = calendar.get(Calendar.MONTH);
-      final int day = calendar.get(Calendar.DAY_OF_MONTH);
+      year = calendar.get(Calendar.YEAR);
+      month = calendar.get(Calendar.MONTH);
+      day = calendar.get(Calendar.DAY_OF_MONTH);
 
       budgetEditText.setCurrency("€");
       budgetEditText.setSpacing(true);
 
-      startDateEditText.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-          DatePickerDialog datePickerDialog = new DatePickerDialog(AddCosplay.this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-              month = month + 1;
-              String date = dayOfMonth + "/" + month + "/" + year;
-              startDateEditText.setText(date);
-            }
-          }, year, month, day);
-          datePickerDialog.show();
-        }
-      });
-     dueDateEditText.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View v) {
-
-         DatePickerDialog datePickerDialog = new DatePickerDialog(AddCosplay.this, new DatePickerDialog.OnDateSetListener() {
-           @Override
-           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-             month = month + 1;
-             String date = dayOfMonth + "/" + month + "/" + year;
-             dueDateEditText.setText(date);
-           }
-         }, year, month, day);
-         datePickerDialog.show();
-       }
-     });
-
-      List<String> statusses = new ArrayList<String>();
+      statusses = new ArrayList<>();
       statusses.add("In porgress");
       statusses.add("Planned");
 
-      startDateEditText.setText(GetCurrentDay());
+      startDateEditText.setText(getCurrentDay());
 
-      ArrayAdapter<String> adapterSpinnerStatus = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, statusses);
-
+      ArrayAdapter<String> adapterSpinnerStatus = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, statusses);
       statusSpinner.setAdapter(adapterSpinnerStatus);
 
-      Button addCosplayBTN = (Button) findViewById(R.id.addCosplayBTN);
-      addCosplayBTN.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-            Log.d(TAG, "onClick: clicked cancelBUttonBTN");
-            SimpleDateFormat sdf = new SimpleDateFormat("dd//MM/yyyy");
-
-            if(characterEditText.getText().toString().isEmpty()) {
-               AlertDialog alertDialog = new AlertDialog.Builder(AddCosplay.this).create();
-               alertDialog.setTitle("Oh No");
-               alertDialog.setMessage("Character name is required to be filled in");
-               alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
-                  @Override
-                  public void onClick(DialogInterface dialog, int which) {
-                  }
-               });
-               alertDialog.show();
-//               Snackbar mySnackbar = Snackbar.make(coordinatorLayout,R.string.message, Snackbar.LENGTH_LONG);
-//               mySnackbar.show();
-            } else  {
-               Cosplay newCosplay = new Cosplay();
-               newCosplay.Name = characterEditText.getText().toString();
-
-               if (!seriesEditText.getText().toString().isEmpty())
-               {
-                  newCosplay.Serie = seriesEditText.getText().toString();
-               }
-
-               if(!startDateEditText.getText().toString().isEmpty())
-               {
-                  try {
-                     newCosplay.StartDate = sdf.parse(startDateEditText.getText().toString());
-                  } catch (ParseException e) {
-                     e.printStackTrace();
-                  }
-               }
-               else
-               {
-                 String date = GetCurrentDay();
-                 //save
-               }
-
-               if(!dueDateEditText.getText().toString().isEmpty()) {
-                  try {
-                     newCosplay.DueDate = sdf.parse(dueDateEditText.getText().toString());
-                  } catch (ParseException e) {
-                     e.printStackTrace();
-                  }
-               }
-
-               if (!budgetEditText.getText().toString().isEmpty())
-               {
-                  newCosplay.Budget = Integer.parseInt(budgetEditText.getText().toString());
-
-               }
-
-               if (!statusSpinner.getSelectedItem().toString().isEmpty())
-                  {
-                     newCosplay.Status = statusSpinner.getSelectedItem().toString();
-                  }
-               //Log.d(TAG, newCosplay.Status);
-               Intent intent = new Intent(AddCosplay.this, MainActivity.class);
-               startActivity(intent);
-            }
-         }
-      });
+      cancelButton.setOnClickListener(v -> onClickCancelButton());
+      startDateEditText.setOnClickListener(v -> onClickStartDate());
+      dueDateEditText.setOnClickListener(v -> onClickdueDate());
+      addCosplayButton.setOnClickListener(v -> onClickAddButton());
    }
 
-   private String GetCurrentDay()
+  private void onClickAddButton() {
+    Log.d(TAG, "onClick: clicked cancelBUttonBTN");
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    if(characterEditText.getText().toString().isEmpty()) {
+      AlertDialog alertDialog = new AlertDialog.Builder(AddCosplay.this).create();
+      alertDialog.setTitle("Oh No");
+      alertDialog.setMessage("Character name is required to be filled in");
+      alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", (dialog, which) -> { });
+      alertDialog.show();
+    } else {
+      Cosplay newCosplay = new Cosplay();
+      newCosplay.Name = characterEditText.getText().toString();
+
+      if (!seriesEditText.getText().toString().isEmpty())
+      {
+        newCosplay.Serie = seriesEditText.getText().toString();
+      }
+
+      if(!startDateEditText.getText().toString().isEmpty())
+      {
+        try {
+          newCosplay.StartDate = sdf.parse(startDateEditText.getText().toString());
+        } catch (ParseException e) {
+          e.printStackTrace();
+        }
+      } else{
+        String date = getCurrentDay();
+      }
+
+      if(!dueDateEditText.getText().toString().isEmpty()) {
+        try {
+          newCosplay.DueDate = sdf.parse(dueDateEditText.getText().toString());
+        } catch (ParseException e) {
+          e.printStackTrace();
+        }
+      }
+
+      if (!budgetEditText.getText().toString().isEmpty())
+      {
+        newCosplay.Budget = Integer.parseInt(budgetEditText.getText().toString());
+
+      }
+
+      if (!statusSpinner.getSelectedItem().toString().isEmpty())
+      {
+        newCosplay.Status = statusSpinner.getSelectedItem().toString();
+      }
+      Intent intent = new Intent(AddCosplay.this, MainActivity.class);
+      startActivity(intent);
+    }
+  }
+
+  private void onClickdueDate() {
+    DatePickerDialog datePickerDialog = new DatePickerDialog(AddCosplay.this, new DatePickerDialog.OnDateSetListener() {
+      @Override
+      public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        month = month + 1;
+        String date = dayOfMonth + "/" + month + "/" + year;
+        dueDateEditText.setText(date);
+      }
+    }, year, month, day);
+    datePickerDialog.show();
+  }
+
+  private void onClickStartDate() {
+    DatePickerDialog datePickerDialog = new DatePickerDialog(AddCosplay.this, new DatePickerDialog.OnDateSetListener() {
+      @Override
+      public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        month = month + 1;
+        String date = dayOfMonth + "/" + month + "/" + year;
+        startDateEditText.setText(date);
+      }
+    }, year, month, day);
+    datePickerDialog.show();
+  }
+
+   private void onClickCancelButton(){
+     Log.d(TAG, "onClick: clicked cancelButton");
+     Intent intent = new Intent(AddCosplay.this, MainActivity.class);
+     startActivity(intent);
+   }
+
+   private String getCurrentDay()
    {
      Date c = Calendar.getInstance().getTime(); //get current date
      System.out.println("Current time => " + c);
