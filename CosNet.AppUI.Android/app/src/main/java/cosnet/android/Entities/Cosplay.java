7eba6a12939f43121f117f314abff1164950392e
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-10-21 16:40:21 +0200

    Update Cosplay.java

--- CosNet.AppUI.Android/app/src/main/java/cosnet/android/Entities/Cosplay.java
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/Entities/Cosplay.java
@@ -5,9 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
-
 import java.util.UUID;
-
 
 @Entity
 public class Cosplay {
