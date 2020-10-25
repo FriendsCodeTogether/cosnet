cfcfebcb810d5b9edd958cff2fbcffd92854cb35
Kaatje Schillemans <kaatschillemans@hotmail.com>
2020-10-25 20:44:32 +0100

    changed program.cs

--- CosNet.API/Program.cs
+++ CosNet.API/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using CosNet.API.Data.DBContexts;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,7 +12,47 @@ namespace CosNet.API
     {
         public static void Main(string[] args)
         {
+            var seed = args.Contains("/seed");
+            if (seed)
+            {
+                args = args.Except(new[] { "/seed" }).ToArray();
+            }
+
             var host = CreateHostBuilder(args).Build();
+
+            if (seed)
+            {
+                SeedDatabase(host);
+                return;
+            }
+
+            Console.WriteLine("Starting host...");
+
+            MigrateDatabase(host);
+
+            host.Run();
+        }
+
+        private static void MigrateDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception)
+                {
+                    //We Could log this in a real world situation
+                }
+            }
+        }
+
+        private static void SeedDatabase(IHost host)
+        {
+            Console.WriteLine("Seeding database...");
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
@@ -25,8 +67,7 @@ namespace CosNet.API
                     //We Could log this in a real world situation
                 }
             }
-
-            host.Run();
+            Console.WriteLine("Done seeding database.");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
