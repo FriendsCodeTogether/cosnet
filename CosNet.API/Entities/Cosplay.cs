4e4aa125f74a455dc968c2b22849783da1ee5032
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-10-25 14:43:52 +0100

    added data annotations to cosplay entity

--- CosNet.API/Entities/Cosplay.cs
+++ CosNet.API/Entities/Cosplay.cs
@@ -15,12 +15,17 @@ namespace CosNet.API.Entities
         public Guid UserId { get; set; }
 
         [Required]
+        [MinLength(1)]
+        [MaxLength(150)]
         public string Name { get; set; }
-
+        [MaxLength(150)]
         public string Serie { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime DueDate { get; set; }
+
+        [Column(TypeName = "decimal(16,3)")]
         public decimal Budget { get; set; }
+        [MaxLength(50)]
         public string Status { get; set; }
     }
 }
