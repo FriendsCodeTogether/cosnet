1ebde8af9da53b7571966b08930342533e70ca38
Tim Blokken <tim_blokken@hotmail.com>
2020-10-25 19:44:16 +0100

    put statussen in strings.xml instead of hardcoded strings

--- CosNet.AppUI.Android/app/src/main/java/cosnet/android/AddCosplay.java
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/AddCosplay.java
@@ -66,8 +66,8 @@ public class AddCosplay extends Activity {
     budgetEditText.setSpacing(true);
 
     statusses = new ArrayList<>();
-    statusses.add("In porgress");
-    statusses.add("Planned");
+    statusses.add(getApplicationContext().getString(R.string.In_Progess));
+    statusses.add(getApplicationContext().getString(R.string.Planned));
 
     ArrayAdapter<String> adapterSpinnerStatus = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, statusses);
     statusSpinner.setAdapter(adapterSpinnerStatus);
