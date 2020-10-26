482d2c5b6cf6f37af28d428dd047c42b6e445d68
Tim Blokken <tim_blokken@hotmail.com>
2020-10-26 17:40:34 +0100

    show cosplay design

---
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/ShowCosplay.java
@@ -0,0 +1,14 @@
+package cosnet.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowCosplay extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_show_cosplay);
+  }
+}
