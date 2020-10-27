b0563e743506ff7711a5d92cef1c71bc9229808b
Kaatje Schillemans <kaatschillemans@hotmail.com>
2020-10-27 10:54:44 +0100

    added showcowplay_menu

--- CosNet.AppUI.Android/app/src/main/java/cosnet/android/ShowCosplay.java
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/ShowCosplay.java
@@ -3,6 +3,8 @@ package cosnet.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ShowCosplay extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ShowCosplay extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_show_cosplay);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.showcosplay_menu,menu);
+    return true;
   }
 }
