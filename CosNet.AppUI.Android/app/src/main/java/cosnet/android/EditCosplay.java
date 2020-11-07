10453052bf10127e7061772dba3121cb2f01d100
Kaatje Schillemans <kaatschillemans@hotmail.com>
2020-11-07 21:54:21 +0100

    fixed consistency

--- CosNet.AppUI.Android/app/src/main/java/cosnet/android/EditCosplay.java
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/EditCosplay.java
@@ -7,12 +7,9 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import java.io.Serializable;
