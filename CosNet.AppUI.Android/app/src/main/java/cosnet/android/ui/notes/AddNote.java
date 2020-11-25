dcd1e5f8e2dd00016348aa46ec9e461bfe5c6a71
Kaatje Schillemans <r0702414@ucll.be>
2020-11-25 09:11:39 +0100

    added funcitonality for addNote

--- CosNet.AppUI.Android/app/src/main/java/cosnet/android/ui/notes/AddNote.java
+++ CosNet.AppUI.Android/app/src/main/java/cosnet/android/ui/notes/AddNote.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 import cosnet.android.CosnetDb;
 import cosnet.android.Entities.Cosplay;
 import cosnet.android.Entities.CosplayItem;
@@ -20,6 +22,8 @@ public class AddNote extends AppCompatActivity {
   private CosnetDb db;
   private CosplayItem cosplayItem;
   private Button addNoteButton;
+  private TextInputLayout noteNameLayout;
+  private TextInputLayout noteDescriptionLayout;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -48,20 +52,60 @@ public class AddNote extends AppCompatActivity {
   }
 
   private void getItemsBound() {
+    addNoteButton = (Button) findViewById(R.id.NoteAddButton);
+    noteNameLayout = (TextInputLayout) findViewById(R.id.noteNametextInput);
+    noteDescriptionLayout = (TextInputLayout) findViewById(R.id.noteDescriptionTextInput);
   }
 
   private void initializeItems() {
- //   addNoteButton = findViewById(R.id.NoteAddButton);
+
   }
 
   private void setListeners() {
-    addNoteButton.setOnClickListener(i -> onClickAddButton());
+    addNoteButton.setOnClickListener(v -> onClickAddButton());
+  }
+
+  private boolean validateItemDescrition() {
+    String description = noteDescriptionLayout.getEditText().getText().toString();
+    if (description.length() > 650) {
+      noteDescriptionLayout.setError(getApplicationContext().getString(R.string.max650Characters));
+      return false;
+    } else {
+      noteDescriptionLayout.setError(null);
+      return true;
+    }
+  }
+
+  private boolean validateItemName() {
+    String itemName = noteNameLayout.getEditText().getText().toString();
+    if (itemName.length() > 150) {
+      noteNameLayout.setError(getApplicationContext().getString(R.string.max150Characters));
+      return false;
+    } else if(itemName.isEmpty()){
+      noteNameLayout.setError(getApplicationContext().getString(R.string.characterNameErrorEmpty));
+      return false;
+    }
+    else {
+      noteNameLayout.setError(null);
+      return true;
+    }
   }
 
   private void onClickAddButton() {
+    if (!validateItemName() | !validateItemDescrition()) {
+      return;
+    }
     CosplayItemNote newNote = new CosplayItemNote();
 
+    newNote.cosplayItemId = cosplayItem.itemId;
+    newNote.title = noteNameLayout.getEditText().getText().toString();
+    newNote.description= noteDescriptionLayout.getEditText().getText().toString();
 
+    db.getCosplayItemNoteDAO().insertItem(newNote);
+
+    Intent intent = new Intent();
+    setResult(RESULT_OK,intent);
+    finish();
   }
 }
 
