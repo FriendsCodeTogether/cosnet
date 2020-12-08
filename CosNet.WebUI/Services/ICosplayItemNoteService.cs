fa42f7abc04aefc50b45c876bba0b0835a10b151
ChaosShay <ChaosShay@outlook.com>
2020-12-08 14:37:48 +0100

    Item Notes Completed

---
+++ CosNet.WebUI/Services/ICosplayItemNoteService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CosNet.Shared.DTOs.CosplayItemNote;
+
+namespace CosNet.WebUI.Services
+{
+    public interface ICosplayItemNoteService
+    {
+        Task CreateCosplayItemNoteAsync(Guid cosplayItemId, CosplayItemNoteForCreationDTO cosplayItemNote);
+        Task DeleteCosplayItemNoteAsync(Guid cosplayItemId, Guid cosplayItemNoteId);
+        Task<CosplayItemNoteDTO> GetCosplayItemNoteAsync(Guid cosplayItemId, Guid cosplayItemNoteId);
+        Task<IEnumerable<CosplayItemNoteDTO>> GetCosplayItemNotesAsync(Guid cosplayItemId);
+        Task UpdateCosplayItemNoteAsync(Guid cosplayItemId, Guid cosplayItemNoteId, CosplayItemNoteForUpdateDTO cosplayItemNote);
+    }
+}
